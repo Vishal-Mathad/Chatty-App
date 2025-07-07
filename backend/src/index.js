09da660242ef95b1977197b51298350f3ac03987
vishal mathad <vishalmathad40@gmail.com>
2025-07-07 18:37:52 +0530

    some small changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chatty-app-kappa.vercel.app",
+    origin: "http://localhost:5173", // Update this to your frontend URL
     credentials: true,
   })
 );
@@ -28,10 +28,10 @@ app.use("/api/messages", messageRoutes);
 // Static frontend serving disabled (handled by Vercel):
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+
 
 connectDB();
 
